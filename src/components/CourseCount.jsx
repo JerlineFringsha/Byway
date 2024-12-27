f9db84ed73537923617cc2186c486b3a7874370b
JerlineFringsha <jerlinfringsha12@gmail.com>
2024-12-27 14:50:42 +0530

    font adjustment

--- src/components/CourseCount.jsx
+++ src/components/CourseCount.jsx
@@ -16,17 +16,17 @@ const CourseCount = () => {
 
   return (
     <>
-      <div className="flex flex-wrap justify-center items-center divide-x divide-gray-300 bg-gray-50 py-6 md:py-8 lg:py-10 rounded-lg shadow">
+      <div className="flex flex-wrap justify-center items-center divide-y sm:divide-y-0 sm:divide-x divide-gray-300 bg-gray-50 py-6 md:py-8 lg:py-10 rounded-lg shadow">
         {counts && counts.length > 0 ? (
           counts.map((count, index) => (
             <div
               key={index}
               className="px-4 py-6 md:px-6 lg:px-8 xl:px-10 2xl:px-12 text-center w-full sm:w-1/2 lg:w-1/4"
             >
-              <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-gray-800">
+              <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl font-bold text-gray-800">
                 {count.course}
               </h2>
-              <p className="text-gray-500 text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl">
+              <p className="text-gray-500 text-xs sm:text-sm md:text-base lg:text-lg xl:text-lg 2xl:text-xl">
                 {count.text}
               </p>
             </div>
